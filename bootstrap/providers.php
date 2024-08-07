cdfcddf624be0d954c8ecaf77b613857a8712382
Essa Sabbagh <50638748+essasabbagh@users.noreply.github.com>
2024-08-08 00:10:20 +0300

    Add telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
